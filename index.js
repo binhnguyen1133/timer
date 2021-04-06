74a9ca46acf4d15aa10a53de42d78bf67c38b789
BinhNguyen <95quangbinhndu1314@gmail.com>
2021-04-06 10:53:41 +0700

    add real time

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const day = [
   "Sunday",
 ];
 
-const date = new Date();
+var date = new Date();
 const month = months[date.getMonth()];
 const dayName = day[date.getDay() - 1];
 const dayNum = date.getDate();
@@ -41,3 +41,18 @@ getDayNum.innerHTML = dayNum;
 const getYear = document.getElementsByClassName("year")[0];
 getYear.innerHTML = year;
 
+function time() {
+  var d = new Date();
+  var hour = document.getElementsByClassName("hour")[0];
+  var minute = document.getElementsByClassName("minute")[0];
+  var second = document.getElementsByClassName("second")[0];
+  var h = d.getHours();
+  var m = d.getMinutes();
+  var s = d.getSeconds();
+
+  hour.innerHTML = h;
+  minute.innerHTML = m;
+  second.innerHTML = s;
+}
+
+var interval = setInterval(time,1000);
